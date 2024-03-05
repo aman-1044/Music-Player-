b2fabe9e00b8867771b6c4af03e79531c62a0d92
Aman Anand <133210252+aman-1044@users.noreply.github.com>
2024-03-05 21:25:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const audioPlayer = document.getElementById('audioPlayer');
+
+    // You can add event listeners or additional functionality here.
+});
